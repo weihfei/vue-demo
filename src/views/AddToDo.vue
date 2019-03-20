2d0432109b98b0186a82582712f27b694896dbc9
weihfa <weihfa@digitalchina.com>
2019-03-20 10:03:38 +0800

    :sparkles: 新增内容

--- src/views/AddToDo.vue
+++ src/views/AddToDo.vue
@@ -2,7 +2,7 @@
   <div class="content-box">
 
      <div class="my-inputbox">
-        <textarea name="my-inputbox" rows="10" v-model="inputToDoContent" placeholder="-请填写内容" v-on:keyup.enter="getCount()"></textarea>
+        <textarea name="my-inputbox" rows="10" v-model="inputToDoContent" placeholder="-请填写内容"></textarea>
         <button type="button" id="confim" v-on:click="getCount()">确定</button>
         <label for="confim">确定</label>
      </div>
@@ -10,7 +10,7 @@
      <div class="my-content-list">
         <div v-for="(item, index) in toDoContentList" :key="index" class="content-item" v-bind:style="{background:randomColor[Math.round(Math.random()*randomColor.length-1)]}">
            <span>{{index+1+'.'}} {{item.content}}</span>
-           <span>{{item.date}}</span>
+           <span>{{item.date}} <i v-on:click="removeList(index)">X</i></span>
         </div>
      </div>
 
@@ -30,18 +30,18 @@ export default class AddToDo extends Vue {
  public inputToDoContent = ''; // 输入内容
  public toDoContentList: any = []; // 列表内容
  public bgColor: any;
- public randomColor = ['#ff2c2c','#ff802c','#ffd82c','#9cdb69','#69db9c','#69cedb','#b5c7f8','#dab5f8','#e694d8']; // 保存随机颜色
+ public randomColor = ['#ff2c2c', '#ff802c', '#ffd82c', '#9cdb69', '#69db9c', '#69cedb', '#b5c7f8', '#dab5f8', '#e694d8']; // 保存随机颜色
 
 public getCount(): void {
-  if (this.inputToDoContent!=='') {// 将填写的内容保存进入数组，用来显示
+  if (this.inputToDoContent) {// 将填写的内容保存进入数组，用来显示
     this.getRandomColor();
     this.toDoContentList.push({content: this.inputToDoContent, date: new Date().toLocaleString()});
     this.inputToDoContent = ''; // 将文本域的内容置为空
   }
 }
 
-//生成随机颜色
-public getRandomColor(){
+// 生成随机颜色
+public getRandomColor() {
   // let colorArray = []; // 申明一个保存颜色的数组
   // for(let i=0; i<3; i++) {
   //   colorArray.push(Math.round(Math.random()*255));
@@ -52,8 +52,13 @@ public getRandomColor(){
   //  }
   //  console.log('打印随机颜色',colorArray);
 
-  this.bgColor=this.randomColor[Math.round(Math.random()*this.randomColor.length)];
-  console.log(this.bgColor,Math.round(Math.random()*this.randomColor.length));
+  this.bgColor = this.randomColor[Math.round(Math.random() * this.randomColor.length)];
+  console.log(this.bgColor, Math.round(Math.random() * this.randomColor.length));
+}
+
+// 删除列表项
+public removeList(I: number) {
+  this.toDoContentList.splice(I, 1);
 }
 
 }
@@ -112,8 +117,16 @@ public getRandomColor(){
   padding: 15px 20px;
   justify-content: space-between;
   margin: 10px auto;
+  cursor: pointer;
   span:last-child{
     justify-content: right;
+  }
+  i{
+    border: 1px solid #353535;
+    color: #353535;
+    border-radius: 25px;
+    padding: 2px 8px;
+    margin-left: 10px;
   }
 }
 
